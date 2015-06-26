f5cc774c925fd4113ac49c93790f035b15239671
Andreas Stephanides <andreas.stephanides@gmail.com>
2015-06-26 16:03:06 +0200

    AutoCommit Fre Jun 26 16:03:06 CEST 2015

--- app/models/lecturer.rb
+++ app/models/lecturer.rb
@@ -14,6 +14,7 @@
 class Lecturer < ActiveRecord::Base
   attr_accessible :email, :name, :oid, :picture, :remove_picture, :picture_cache, :lva_ids, :link
   has_and_belongs_to_many :lvas
+  has_many :beispiele
   mount_uploader :picture, PictureUploader
   resourcify
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,8 +15,8 @@ Fetsite::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = true
- config.action_mailer.delivery_method =:sendmail
- config.action_mailer.default_url_options = {} #  :host => 'glonass.htu.tuwien.ac.at' }
+  config.action_mailer.delivery_method =:sendmail
+  config.action_mailer.default_url_options = {} #  :host => 'glonass.htu.tuwien.ac.at' }
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 
