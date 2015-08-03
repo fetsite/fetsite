d88edd708d69ce63094c9de9f5814b0233db4248
Andreas Stephanides <andreas.stephanides@gmail.com>
2015-08-03 09:03:01 +0200

    AutoCommit Mon Aug  3 09:03:01 CEST 2015

--- db/migrate/20150803060145_add_template_to_survey_question.rb
+++ db/migrate/20150803060145_add_template_to_survey_question.rb
@@ -1,4 +1,9 @@
 class AddTemplateToSurveyQuestion < ActiveRecord::Migration
   def change
+    add_column :survey_questions, :flag_template, :boolean
+    add_column :survey_questions, :flag_deleted, :boolean
+    add_column :survey_questions, :flag_intern,:boolean
+    add_column :survey_questions, :user_id, :integer
+    add_column :survey_questions, :flag_locked, :boolean
   end
 end
