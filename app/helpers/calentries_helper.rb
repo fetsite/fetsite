8b0e655a88ebdc58a0ec318507a821219aa4a6b0
Andreas Stephanides <andreas.stephanides@gmail.com>
2015-08-19 23:03:02 +0200

    AutoCommit Mit Aug 19 23:03:02 CEST 2015

--- app/helpers/calentries_helper.rb
+++ app/helpers/calentries_helper.rb
@@ -3,4 +3,7 @@ module CalentriesHelper
     calentry_list=p.calentries
     render(partial: "calentries/calentry_list", object: calentry_list, locals:{parent: p})
   end
+  def new_calentry_div(p)
+    content_tag("div", "", id: divid_for(p, "new_calentry"))
+  end
 end
